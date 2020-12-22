08404a5932e04d5a4a2ec6b9ba67d20108d49fa3
Dayeon Choi <75402494+dayomi02@users.noreply.github.com>
2020-12-22 23:25:30 +0900

    php file upload

---
+++ php/update.php
@@ -0,0 +1,38 @@
+<?php  
+require_once("conn.php");
+session_start();
+
+$filtered=array(
+	'id'=>mysqli_real_escape_string($conn,$_GET['id']),
+    'user_id'=>mysqli_real_escape_string($conn,$_SESSION['user_id']),
+	// content id
+);
+$sql="select * from content where id={$filtered['id']}";
+$result=mysqli_query($conn,$sql);
+$row=mysqli_fetch_array($result);
+$escaped_title=htmlspecialchars($row['title']);
+$escaped_description=htmlspecialchars($row['description']);
+$escaped_kinds=htmlspecialchars($row['kinds']);
+$formhref="update_process.php";
+$img_src=$escaped_kinds;
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+    <link href="css/cookbread.css" rel="stylesheet">   
+</head>
+ <body onload="alerted_selected_value('<?=$escaped_kinds?>')">
+<?php
+    require_once("lib/write.php");
+?>
+            <input type="hidden" name="content_id" value="<?=$filtered['id']?>">
+            <input type="hidden" name="user_id" value="<?=$filtered['user_id']?>">
+            <input type="submit" id="submitButton" name="submit" alt="빵 굽기!" value="">
+        </div>
+    </form>
+
+    <script src="js/cookbread.js"></script>
+</body>
+</html>
